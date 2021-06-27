677e283f885c605864ba2fd9da88650b57d87b1c
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2021-06-27 12:53:43 -0500

    added the queries

--- 01_SELECT_basics.sql
+++ 01_SELECT_basics.sql
@@ -1,6 +1,11 @@
---Show the population of Germany
-
+-- 1. Show the population of Germany
 SELECT population FROM world
   WHERE name = 'Germany'
+  
+-- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
-
+-- 3. Show the country and the area for countries with an area between 200,000 and 250,000
+SELECT name, area FROM world
+WHERE area> 200000 and area < 250000
