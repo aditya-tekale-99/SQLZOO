4478bbfb3bdff1186d8a4134b648732b3a2ee2f1
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2021-06-29 18:19:54 -0500

    added all the queries for select within select.

--- 04_SELECT_within_SELECT_tutorial.sql
+++ 04_SELECT_within_SELECT_tutorial.sql
@@ -42,28 +42,38 @@ WHERE population IN (SELECT population
                      FROM world WHERE continent='Europe');
 
 -- 6. Which countries have a GDP greater than every country in Europe? 
+SELECT name FROM world 
+WHERE gdp > ALL(SELECT gdp FROM world 
+WHERE continent = 'Europe' AND gdp IS NOT NULL);
 
+-- 7. Find the largest country (by area) in each continent, show the continent, 
+--    the name and the area
+SELECT x.continent, x.name, x.area
+FROM world AS x
+WHERE x.area = (SELECT MAX(y.area) FROM world AS y
+WHERE x.continent = y.continent);
 
+-- 8. List each continent and the name of the country that comes first alphabetically.
+Select  x.continent, x.name
+From world x
+Where x.name <= ALL (select y.name from world y where x.continent=y.continent)
+ORDER BY name;
 
+-- 9. Find the continents where all countries have a population <= 25000000. 
+--    Then find the names of the countries associated with these continents. 
+--    Show name, continent and population.
+SELECT a.name, a.continent, a.population FROM world AS a
+	WHERE 25000000 > ALL(
+		SELECT b.population FROM world AS b
+			WHERE b.continent = a.continent
+	);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- 10. Some countries have populations more than three times that of any of their 
+--     neighbours (in the same continent). Give the countries and continents.
+SELECT a.name, a.continent FROM world AS a
+	WHERE a.population/3 > ALL(
+		SELECT b.population FROM world AS b
+			WHERE a.continent = b.continent AND
+			a.name <> b.name
+	);
 
